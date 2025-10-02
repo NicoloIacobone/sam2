c8b73c4eabdfee92b6fbd7137da7bee119e9a5f8
NICOLO' IACOBONE s317025 <nicolo.iacobone@gmail.com>
2025-10-02 12:16:48 +0200

    aggiunta modifica anche all'else dell'if sbagliato della push precedente

--- sam2/modeling/backbones/image_encoder.py
+++ sam2/modeling/backbones/image_encoder.py
@@ -57,6 +57,7 @@ class ImageEncoder(nn.Module):
             elif len(vision_features_shape) == 4 and vision_features_shape[0] == 1:
                 # 1 x B x H x W
                 output["vision_features"] = loaded_feature.unsqueeze(0)
+                output["backbone_fpn"][-1] = loaded_feature.unsqueeze(0)
                 print(f"[DEBUG] Replaced vision_features with loaded feature of shape: {loaded_feature.unsqueeze(0).shape}")
             else:
                 raise ValueError(f"Unexpected shape for vision_features: {vision_features_shape}")
