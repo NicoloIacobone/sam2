9f9bc8960860e905cf6f92227d592d3c00b85243
NICOLO' IACOBONE s317025 <nicolo.iacobone@gmail.com>
2025-10-16 14:13:19 +0200

    test case - consistency test for run_1

--- sam2/modeling/backbones/image_encoder.py
+++ sam2/modeling/backbones/image_encoder.py
@@ -43,7 +43,8 @@ class ImageEncoder(nn.Module):
         consistency_test = True
         # debug_vision_features_path = "/cluster/work/igp_psr/niacobone/distillation/mapanything/original/student_embeddings_epoch1000.pt"  # Modifica questo path per il debug
         # debug_vision_features_path = "/cluster/work/igp_psr/niacobone/distillation/mapanything/original/student_embeddings_epoch3000.pt"  # Modifica questo path per il debug
-        debug_vision_features_path = "/cluster/work/igp_psr/niacobone/distillation/mapanything/original/student_embeddings_epoch5000.pt"  # Modifica questo path per il debug
+        # debug_vision_features_path = "/cluster/work/igp_psr/niacobone/distillation/mapanything/original/student_embeddings_epoch5000.pt"  # Modifica questo path per il debug
+        debug_vision_features_path = "/cluster/work/igp_psr/niacobone/distillation/mapanything/original/student_embeddings_run_1.pt"  # Modifica questo path per il debug
         if consistency_test:
             loaded = torch.load(debug_vision_features_path, map_location=src.device, weights_only=True)
 
