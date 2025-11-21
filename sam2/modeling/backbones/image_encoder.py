01596bd93a2e92a7bb98337e69286bfecaa4bf58
NICOLO' IACOBONE s317025 <nicolo.iacobone@gmail.com>
2025-11-21 11:50:14 +0100

    test case - student emebddings

--- sam2/modeling/backbones/image_encoder.py
+++ sam2/modeling/backbones/image_encoder.py
@@ -42,7 +42,7 @@ class ImageEncoder(nn.Module):
         }
 
         # DEBUG: CONSISTENCY TEST
-        consistency_test = False
+        consistency_test = True
         debug_vision_features_path = "/cluster/work/igp_psr/niacobone/distillation/output/tests/embeddings/student/000000000030.pt"  # Modifica questo path per il debug
         if consistency_test:
             loaded = torch.load(debug_vision_features_path, map_location=src.device, weights_only=True)
