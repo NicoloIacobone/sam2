aa9465e780bff562bfb401bcac55351163f6f5c8
NICOLO' IACOBONE s317025 <nicolo.iacobone@gmail.com>
2025-11-25 12:37:10 +0100

    test case - teacher masks

--- sam2/modeling/backbones/image_encoder.py
+++ sam2/modeling/backbones/image_encoder.py
@@ -42,7 +42,7 @@ class ImageEncoder(nn.Module):
         }
 
         # DEBUG: CONSISTENCY TEST
-        student = True
+        student = False
         # debug_vision_features_path = "/cluster/work/igp_psr/niacobone/distillation/tests/embeddings/student/000000002587.pt"  # Modifica questo path per il debug
         debug_vision_features_path = "/cluster/work/igp_psr/niacobone/distillation/output/distillation_4/visualizations/student/000000002587.pt"  # Modifica questo path per il debug
         if student:
