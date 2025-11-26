3226ca95e68a5ed376becf8c911b8e7d105bf528
NICOLO' IACOBONE s317025 <nicolo.iacobone@gmail.com>
2025-11-26 19:34:43 +0100

    test case - consistency overfit 1k imgs teacher

--- sam2/modeling/backbones/image_encoder.py
+++ sam2/modeling/backbones/image_encoder.py
@@ -42,9 +42,9 @@ class ImageEncoder(nn.Module):
         }
 
         # DEBUG: CONSISTENCY TEST
-        student = True
+        student = False
         # debug_vision_features_path = "/cluster/work/igp_psr/niacobone/distillation/tests/embeddings/student/000000002587.pt"  # Modifica questo path per il debug
-        debug_vision_features_path = "/cluster/work/igp_psr/niacobone/distillation/output/overfit_single_image_test_4/visualizations/student/599.pt"  # Modifica questo path per il debug
+        debug_vision_features_path = "/cluster/work/igp_psr/niacobone/distillation/output/overfit_single_image_test_5/visualizations/student/199.pt"  # Modifica questo path per il debug
         if student:
             loaded = torch.load(debug_vision_features_path, map_location=src.device, weights_only=True)
 
